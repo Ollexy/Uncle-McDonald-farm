cbc29a947b19b2e606f34da1d78092ad74ef5b40
Rafal Oleksiewicz <70261243+Ollexy@users.noreply.github.com>
2022-04-02 16:51:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-void solve(long double fenceLenght);
+void solve(long double fenceLength);
 
 int main()
 {
-    long double fenceLenght, area, a, b;
+    long double fenceLength;
     string start;
 
     //starting the algorithm by entering "run"
@@ -18,35 +18,36 @@ int main()
     }
     while (start != "run");
 
-    //Fence lenght
+    //Fence length
     cout << "How many meters of fence is avaible: ";
-    cin >> fenceLenght;
+    cin >> fenceLength;
 
     //checking if x >= 0 or if it's not a letter
-    while ((fenceLenght < 0) || (!cin))
+    while ((fenceLength < 0) || (!cin))
     {
-        cout << "Given lenght of fence is not avaible. Try again." << endl;
+        cout << "Given length of fence is not avaible. Try again." << endl;
         cout << "How many meters of fence is avaible: ";
         cin.clear();
         cin.ignore(100, '\n');
-        cin >> fenceLenght;
+        cin >> fenceLength;
     }
-    solve(fenceLenght);
+    solve(fenceLength);
 
     return 0;
 }
 
-void solve(long double fenceLenght)
+void solve(long double fenceLength)
 {
     long double area, a, b;
     //optimal area of farm
-    fenceLenght = floor(fenceLenght); // rounding down, because for example, with 10.5 m of fence, we can't use 11 m
-    a = round( fenceLenght/4 ); // axis symetry ( lenght = -x/2(lenght) = -x/2(-2))
-    b = fenceLenght - 2 * a;
+    fenceLength = floor(fenceLength); // rounding down, because for example, with 10.5 m of fence, we can't use 11 m
+    a = round( fenceLength/4 ); // axis symetry || area =a*b, fence = 2a+b, a=fence-2a, area = (fence-2a)a, area=-2a*a + fence(a), fence-2a = 0, fence=2a, a=fence/2(it is a root, so axis symetry is in half, so a= fence/4
+    b = fenceLength - 2 * a;
     area = a * b;
 
     //returned values
     cout << "Area: " << area << endl;
-    cout << "Lenght \"a\": " << a << endl;
+    cout << "Length \"a\": " << a << endl;
     cout << "Width \"b\": " << b << endl;
 }
+
